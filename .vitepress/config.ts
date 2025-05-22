514f648a61fcc51c543153bf40b2b9b957fb9970
Kash <ipomainkra@gmail.com>
2025-05-22 15:45:40 +0300

    Edit socialLinks, nav and sidebar && Add editLink, lastUpdated

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,27 +2,46 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'ru-RU',
   title: "Kamish",
+  base: '/',
   description: "Kamish docs",
+  lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+
+
+    search: {
+      provider: 'local'
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Вернуться на сайт', link: 'https://kamish.pro' }
     ],
+
+    editLink: {
+      pattern: 'https://github.com/kamish522/kamish-docs/:path',
+      text: 'Редактировать страницу на GitHub'
+    },
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Гайды',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Главная', link: '/' },
+        ]
+      },
+      {
+        text: 'Документы',
+        items: [
+          { text: 'Условия использования', link: '/tos' },
+          { text: 'Политика конфиденциальность', link: '/privacy' }
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/kamish522/kamish-docs' },
+      { icon: 'discord', link: 'https://discord.gg/8p8NYhparv' }
     ]
   }
 })
